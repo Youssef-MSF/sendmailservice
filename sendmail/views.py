69fe9f730f568406ae877657452fcccbecdfa8f6
Youssef Moussaaif <63004347+Youssef-MSF@users.noreply.github.com>
2021-02-28 21:43:24 +0100

    Update views.py

--- sendmail/views.py
+++ sendmail/views.py
@@ -5,30 +5,28 @@ from django.http import HttpResponse
 
 # Create your views here.
 def send_mail(request):
-    try:
-        # Sender email
-        sender_email = "linked.clubs@gmail.com"
-        # Receiver email
-        receiver_email = request.GET['rec']
-        # Message to send
-        message = request.GET['message']
+    # Sender email
+    sender_email = "linked.clubs@gmail.com"
+    # Receiver email
+    receiver_email = request.GET['rec']
+    # Message to send
+    message = request.GET['message']
 
-        msg = f'subject: LinkedClubs\n\n{message}'
+    msg = f'subject: LinkedClubs\n\n{message}'
 
-        # Initialize the server
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        # Start the server
-        server.starttls()
+    # Initialize the server
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    # Start the server
+    server.starttls()
 
-        # Login
-        server.login(sender_email, "linkedclubs2020")
-        print("Login success !")
+    # Login
+    server.login(sender_email, "linkedclubs2020")
+    print("Login success !")
 
-        # Send the message
-        server.sendmail(sender_email, receiver_email, msg)
+    # Send the message
+    server.sendmail(sender_email, receiver_email, msg)
 
-        print("Email has been sent successfully !")
+    print("Email has been sent successfully !")
 
-        return HttpResponse('<h1>Sent</h1>')
-    except:
-        return HttpResponse('<h1>Error</h1>')
+    return HttpResponse('<h1>Sent</h1>')
+
